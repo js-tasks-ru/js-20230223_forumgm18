77d327e8877fc6749394d54d1f07602a38d46264
Andrey Shishkin <a.shishkin@holodilnik.ru>
2023-03-06 23:21:46 +0400

    Задачи Модуля 4

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,28 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  if (size === undefined)  return string;
+  if (typeof size === 'number' && size === 0)  return '';
+  if (string.length <= size) return string;
+
+  let 
+    mask = string[0],
+    start = 0;
+
+  const res = [];
+    for( let i = 1; i < string.length; i++) {
+      if (string[i] != mask) {
+        res.push(string.slice(start, i));
+        mask = string[i];
+        start = i;
+      }
+    }
+  res.push(string.slice(start));
+
+  return res.reduce((s, el) => {
+    let mask = el[0].repeat(size);
+    return s + ( el.includes(mask) ? mask : el)
+  },'');
+  
 
 }
--- 03-objects-arrays-intro-to-testing/4-uniq/index.js
+++ 03-objects-arrays-intro-to-testing/4-uniq/index.js
@@ -4,5 +4,10 @@
  * @returns {*[]} - the new array with uniq values
  */
 export function uniq(arr) {
-
+  return (arr instanceof Array) 
+          ? arr.reduce((res, el) => {
+              if (!res.includes(el)) res.push(el);
+              return res;
+            },[]) 
+          : [];
 }
